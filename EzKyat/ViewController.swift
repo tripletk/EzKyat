f5fa053a12d5b56cea2acfc375d74789d896cec8
Kaung Khant Kyaw <timmytseng1999@gmail.com>
2020-01-06 17:18:18 +0800

    Initial Commit

---
+++ EzKyat/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  EzKyat
+//
+//  Created by Timmy Tseng on 1/6/20.
+//  Copyright © 2020 kaungkhantkyaw. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
